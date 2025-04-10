9bf498a866bd1baa308497f4f88c1ef5d5678cd8
NguyetTruong160595 <75405890+NguyetTruong160595@users.noreply.github.com>
2025-04-10 14:31:36 +0700

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    baseURL: 'https://app.secapa.jp/',
+  },
+  projects: [
+    {
+      name: 'API Tests',
+      testMatch: /.*api\.spec\.ts/,
+    },
+    {
+      name: 'UI Tests',
+      testMatch: /.*\.spec\.ts/,
+      grepInvert: /api/, // Exclude API specs from UI project
+    },
+    ],
+  retries: 1,
+});
