32a02858d7d73d4181b18494a555cf8dace1fed9
NguyetTruong16051995 <nguyettruongthiminh@freec.asia>
2025-04-13 14:04:00 +0700

    check code

--- utils/ssh-db.ts
+++ utils/ssh-db.ts
@@ -1,28 +1,28 @@
-import { Client } from 'ssh2';
-import { Pool } from 'pg';
+//import { Client } from 'ssh2';
+//import { Pool } from 'pg';
 
-export async function connectViaSSHTunnel() {
-  const ssh = new Client();
+//export async function connectViaSSHTunnel() {
+//  const ssh = new Client();
 
-  return new Promise<Pool>((resolve, reject) => {
-    ssh.on('ready', () => {
-      ssh.forwardOut('secapa-stg-instance.cja8ouiiyfty.ap-northeast-1.rds.amazonaws.com', 12345, 'dev_secapa', 5432, (err, stream) => {
-        if (err) return reject(err);
+//  return new Promise<Pool>((resolve, reject) => {
+ //   ssh.on('ready', () => {
+//      ssh.forwardOut('secapa-stg-instance.cja8ouiiyfty.ap-northeast-1.rds.amazonaws.com', 12345, 'dev_secapa', 5432, (err, stream) => {
+ //       if (err) return reject(err);
+//
+  //      const pool = new Pool({
+   //       user: 'postgres',
+   //       password: 'SecapaPasswoRD',
+   //       database: 'secapa',
+   //o      stream: () => stream, // 🧠 Connect through the SSH stream
+   //     });
 
-        const pool = new Pool({
-          user: 'postgres',
-          password: 'SecapaPasswoRD',
-          database: 'secapa',
-          stream: () => stream, // 🧠 Connect through the SSH stream
-        });
-
-        resolve(pool);
-      });
-    }).connect({
-      host: '57.180.185.14',
-      port: 22,
-      username: 'ec2-user',
-      //privateKey: require('fs').readFileSync('/Users/nguyettruong/Downloads/secapa-dev'),
-    });
-  });
-}
+   //     resolve(pool);
+  //    });
+  //  }).connect({
+  //    host: '57.180.185.14',
+   //   port: 22,
+   //   username: 'ec2-user',
+   //   //privateKey: require('fs').readFileSync('/Users/nguyettruong/Downloads/secapa-dev'),
+//    });
+ // });
+//}
