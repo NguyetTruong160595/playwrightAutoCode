306c4682693b656dcbff0fe22daa47d0617037f5
NguyetTruong16051995 <nguyettruongthiminh@freec.asia>
2025-04-13 14:14:40 +0700

    check lastest code 1

--- tests/api/user-api.spec.ts
+++ tests/api/user-api.spec.ts
@@ -25,7 +25,7 @@ test('POST login - status 201 and data check', async ({ }) => {
     const response = await apiContext.post('https://api.stg.secapa.jp/auth/login',
     {
         data: {
-            "mail": "nguyettruong+2000@freec.asia",
+            "mail": "nguyettruong+2001@freec.asia",
             "password": "12345678"
         },
     });
@@ -40,7 +40,7 @@ test('POST create user - status 201', async ({ }) => {
   const apiContext = await request.newContext();
   const response = await apiContext.post('https://api.stg.secapa.jp/auth/upsertRegistration', {
     data: {
-        "mail": "nguyettruong+2000@freec.asia",
+        "mail": "nguyettruong+2001@freec.asia",
         "password": "12345678",
         "gender_code": "1",
         "is_terms_consented": true,
@@ -55,5 +55,5 @@ test('POST create user - status 201', async ({ }) => {
   expect(response.status()).toBe(201);
 
   const body = await response.json();
-  expect(body.data).toHaveProperty('mail', "nguyettruong+1008@freec.asia");
+  expect(body.data).toHaveProperty('mail', "nguyettruong+2001@freec.asia");
 });
