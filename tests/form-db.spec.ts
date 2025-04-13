9a0fb04b3e3438f95b0f3d2ee275d9fe97ee5c8f
NguyetTruong16051995 <nguyettruongthiminh@freec.asia>
2025-04-13 14:08:05 +0700

    push code remove DB testcases

--- tests/form-db.spec.ts
+++ tests/form-db.spec.ts
@@ -1,12 +1,12 @@
-import { expect, test } from '@playwright/test';
-import { connectViaSSHTunnel } from '../utils/ssh-db';
+//import { expect, test } from '@playwright/test';
+//import { connectViaSSHTunnel } from '../utils/ssh-db';
 
-test('Check DB via SSH tunnel', async () => {
-  const pool = await connectViaSSHTunnel();
-  const result = await pool.query('SELECT * FROM users WHERE mail = $1', ['nguyettruong+90@freec.asia']);
+//test('Check DB via SSH tunnel', async () => {
+//  const pool = await connectViaSSHTunnel();
+//  const result = await pool.query('SELECT * FROM users WHERE mail = $1', ['nguyettruong+90@freec.asia']);
 
-  expect(result.rows.length).toBe(1);
-  expect(result.rows[0].email).toBe('nguyettruong+90@freec.asia');
+//  expect(result.rows.length).toBe(1);
+//  expect(result.rows[0].email).toBe('nguyettruong+90@freec.asia');
 
-  await pool.end();
-});
+//  await pool.end();
+//});
